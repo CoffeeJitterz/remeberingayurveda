c88196ecf2e3db23cdc92637071df8d9f7c948b4
CoffeeJitterz <josh.millard10@gmail.com>
2022-03-08 18:35:55 -0500

    useState applied to title and description inputs

--- client/src/components/EventPanel.jsx
+++ client/src/components/EventPanel.jsx
@@ -1,11 +1,16 @@
 import {useState} from 'react'
 
 export default function EventPanel() {
+    const [title, setTitle] = useState('Title')
+    const [description, setDescription] = useState('Description')
+    console.log(title, description)
+
+    // const output = {title, description}
   return (
     <div>
         <h3>EventPanel</h3>
-        <input name='Title' placeholder='Title'/>
-        <input name='Description' placeholder='Description'/>
+        <input name='Title' placeholder={title} value={title} onChange={e => {setTitle(e.target.value)}}/>
+        <input name='Description' placeholder={description} value={description} onChange={e => setDescription(e.target.value)}/>
         <h6>upload image</h6>
     </div>
   )
