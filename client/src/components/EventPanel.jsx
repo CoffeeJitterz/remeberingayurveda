eeb9621efb55ebfb3d43c10f92da46fd524af844
CoffeeJitterz <josh.millard10@gmail.com>
2022-03-08 18:12:34 -0500

    Event Panel component created

---
+++ client/src/components/EventPanel.jsx
@@ -0,0 +1,12 @@
+import {useState} from 'react'
+
+export default function EventPanel() {
+  return (
+    <div>
+        <h3>EventPanel</h3>
+        <textarea>Title</textarea>
+        <textarea>Description</textarea>
+        <h6>upload image</h6>
+    </div>
+  )
+}
