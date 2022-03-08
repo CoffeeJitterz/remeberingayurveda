c317de2b12a61309e9c534fd15eb84270d4b3da2
CoffeeJitterz <josh.millard10@gmail.com>
2022-03-08 18:18:14 -0500

    textarea changed to input on eventpanel

--- client/src/components/EventPanel.jsx
+++ client/src/components/EventPanel.jsx
@@ -4,8 +4,8 @@ export default function EventPanel() {
   return (
     <div>
         <h3>EventPanel</h3>
-        <textarea>Title</textarea>
-        <textarea>Description</textarea>
+        <input name='Title' placeholder='Title'/>
+        <input name='Description' placeholder='Description'/>
         <h6>upload image</h6>
     </div>
   )
