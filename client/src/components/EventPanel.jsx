21d66560975031aca278b51692d68e5298fce4af
CoffeeJitterz <josh.millard10@gmail.com>
2022-03-08 19:06:36 -0500

    form and formhandler function added to EventPanel component

--- client/src/components/EventPanel.jsx
+++ client/src/components/EventPanel.jsx
@@ -5,13 +5,25 @@ export default function EventPanel() {
     const [description, setDescription] = useState('Description')
     console.log(title, description)
 
+    const [output, setOutput] = useState({title: 'default', desctiption: 'default'})
+    console.log(output)
+
+    const formHandler = e => {
+        e.preventDefault()
+        setOutput({title, description})
+    }
+
     // const output = {title, description}
   return (
     <div>
+        <form onSubmit={formHandler}>
         <h3>EventPanel</h3>
         <input name='Title' placeholder={title} value={title} onChange={e => {setTitle(e.target.value)}}/>
         <input name='Description' placeholder={description} value={description} onChange={e => setDescription(e.target.value)}/>
         <h6>upload image</h6>
+
+        <input type='submit'/>
+        </form>
     </div>
   )
 }
