1a69cc637f70d6f547abd73484be13e31daa15fc
CoffeeJitterz <josh.millard10@gmail.com>
2022-03-09 20:08:14 -0500

    About Link removed from Header component

--- client/src/shared/Header.jsx
+++ client/src/shared/Header.jsx
@@ -7,9 +7,6 @@ export default function Header() {
         <Link to='/'>
             HOME
         </Link >
-        <Link to='/about'>
-            ABOUT
-        </Link>
         <Link to='/events'>
             EVENTS
         </Link>
