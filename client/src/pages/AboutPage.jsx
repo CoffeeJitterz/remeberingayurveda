9a148d993480929e14fd36697f13f7ac21c371c7
CoffeeJitterz <josh.millard10@gmail.com>
2022-03-15 18:16:37 -0400

    Header added to about page

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,8 +1,10 @@
+import Header from '../shared/Header'
 import About from "../components/About"
 
 export default function AboutPage() {
   return (
     <div>
+        <Header />
         <About expand={true}/>
     </div>
   )
