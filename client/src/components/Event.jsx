10f8ca909a809ed4e9a808d09e4547a8f39bf427
CoffeeJitterz <josh.millard10@gmail.com>
2022-04-03 12:48:19 -0400

    image option added into event posting

--- client/src/components/Event.jsx
+++ client/src/components/Event.jsx
@@ -1,9 +1,15 @@
 import Card from "../shared/Card"
 
+
 export default function Event( { title, date, description, image, events } ) {
+  console.log(image)
+  let picture = null
+  if (image) {
+    const picture = require(`../images/${image}`)
     return (
       <span className='event'>
         <Card>
+          {image && <img src={picture} height={400} />}
           <h3>{title}</h3>
           <h5>{date}</h5>
           <p>{description}</p>
@@ -11,4 +17,15 @@ export default function Event( { title, date, description, image, events } ) {
         </Card>
       </span>
     )
+  }
+  return (
+    <span className='event'>
+      <Card>
+        <h3>{title}</h3>
+        <h5>{date}</h5>
+        <p>{description}</p>
+        <h6>{image}</h6>
+      </Card>
+    </span>
+  )
 }
