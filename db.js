b6c250327d514f5d7023a47852aa717650b8feba
CoffeeJitterz <josh.millard10@gmail.com>
2022-04-23 11:16:15 -0400

    site ready to be deployed

--- db.js
+++ db.js
@@ -1,10 +1,17 @@
 const Pool = require("pg").Pool
+require("dotenv").config()
 
-const pool = new Pool({
-    user: "joshmillard",
-    host:"localhost",
-    database: "remembering_aurveda",
-    port: 5432
-})
+const devConfig = {
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    port: process.env.PG_PORT
+}
+
+const proConfig = {
+    connectionString: process.env.DATABASE_URL
+}
+
+const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig)
 
 module.exports = pool 
--- server.js
+++ server.js
@@ -15,17 +15,19 @@ const port = process.env.PORT || 3001
 app.use(express.json())
 app.use(cors())
 
-app.use(express.static(path.resolve(__dirname, '../client/build')))
+// if (process.env.NODE_ENV === "producion") {
+//     //serve static content
+//     app.use(express.static(path.resolve(__dirname, './client/build')))
+// }
+app.use(express.static(path.resolve(__dirname, './client/build')))
 
 app.get('/api', (req, res) => {
     res.send('Remembering Aurveda')
 })
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
-// })
-
 app.use('/api/events', eventRoutes)
 app.use('/api/blogs', blogRoutes)
 
+app.get('*', (req, res) => {
+    res.redirect('/')
+})
 app.listen(port, () => console.log(`Remembering_Aurveda is running on port ${port}`))
