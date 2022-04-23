ae77c92300ddffed2605478ade47012cb6b2ae20
CoffeeJitterz <josh.millard10@gmail.com>
2022-04-23 19:37:10 -0400

    image added to front page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Services from '../components/Services'
 import Card from '../shared/Card'
 import Bio from '../components/Bio'
 import EventFeed from '../components/EventFeed'
+const image = require('../images/what_is_ayurveda.png')
 
 
 export default function Home( { events } ) {
@@ -10,7 +11,8 @@ export default function Home( { events } ) {
     <div>
         <Header />
         <Card>
-          <h2>What is Ayurveda?</h2>
+          <img src={image} width='1440' alt='alt'/>
+          {/* <h2>What is Ayurveda?</h2>
           <p>Ayurveda is a complete healthcare science, 
             focused on restoring and maintaining a balance 
             of the mind, body, and spirit. The tools of 
@@ -26,7 +28,7 @@ export default function Home( { events } ) {
 
             <p>“The Ayurvedic Route to great health involves 
                 two simple steps - Doing less and being more” 
-            </p>
+            </p> */}
         <Bio expand={false}/>          
         <Services expand={false} />
         {events && <EventFeed expand={null} events={events}/>}
